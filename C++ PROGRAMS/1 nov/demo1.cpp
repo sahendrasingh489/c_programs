0ce7a6da8d9c0fd6b1e9635878edd787a75f6737
sahendrasingh489 <144704384+sahendrasingh489@users.noreply.github.com>
2023-11-01 21:43:49 +0530

    Add files via upload

---
+++ C++ PROGRAMS/1 nov/demo1.cpp
@@ -0,0 +1,10 @@
+// program
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Welcome to C++\n";
+    cout<<"Welcome to C++"<<"\n";
+    cout<<"Welcome to C++"<<endl;
+    cout<<"Welcome to C++";
+}
