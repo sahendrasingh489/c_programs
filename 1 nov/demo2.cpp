9498c2a571cb45f3f3de217999c1f8d47bae4e2c
sahendrasingh489 <144704384+sahendrasingh489@users.noreply.github.com>
2023-11-01 21:40:08 +0530

    Delete 1 nov directory

--- 1 nov/demo2.cpp
+++
@@ -1,9 +0,0 @@
-// program
-
-#include<iostream>
-int main(){
-    std::cout<<"Welcome to C++\n";
-    std::cout<<"Welcome to C++"<<"\n";
-    std::cout<<"Welcome to C++"<<std::endl;
-    std::cout<<"Welcome to C++";
-}
